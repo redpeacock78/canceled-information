8521c209a5a5eb8d3524bb882f23af658c7b6068
redpeacock78 <scarletpeacock78@gmail.com>
2019-07-25 04:53:54 +0900

    Edited content: get-kyuko-news.sh

--- get-kyuko-news.sh
+++ get-kyuko-news.sh
@@ -13,7 +13,16 @@ fi
 if [[ -e ./data.txt ]]; then
   :
 else
-  curl -sS https://www.kyoto-art.ac.jp/student/ | grep -B 1 -A 5 '<p class="time">' | sed 's/<[^>]*>//g;s/ //g' | sed '/^$/d;s/--//g' | sed '/曜/s/^/(/;s/曜/) /' | sed '/：/s/\n/ /g' | tr \\n ' ' | sed 's/  /\n/g;s/$/\n/' | sed 's/担当教員：/ /g;s/・/,/g;s/　//g;y/１２３４５６/123456/' | awk '{print $1$2,$3,"\x5b"$4"\x5d"$5,$6}' > ./data.txt
+  curl -sS https://www.kyoto-art.ac.jp/student/ \
+  | grep -B 1 -A 5 '<p class="time">' \
+  | sed 's/<[^>]*>//g;s/ //g' \
+  | sed '/^$/d;s/--//g' \
+  | sed '/曜/s/^/(/;s/曜/) /' \
+  | sed '/：/s/\n/ /g' \
+  | tr \\n ' ' \
+  | sed 's/  /\n/g;s/$/\n/' \
+  | sed 's/担当教員：/ /g;s/・/,/g;s/　//g;y/１２３４５６/123456/' \
+  | awk '{print $1$2,$3,"\x5b"$4"\x5d"$5,$6}' > ./data.txt
 fi
 
 
